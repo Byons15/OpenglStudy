895eb340b2f2294fa88fa0ebf9dd42931898e8f8
Byons <buyongshi15@163.com>
2021-02-08 01:20:07 +0800

    。

--- Light/main.cpp
+++ Light/main.cpp
@@ -7,8 +7,17 @@
 #include <glm\glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include "Camera.h"
+
+#pragma comment(lib, "legacy_stdio_definitions.lib")
+#pragma comment(lib, "glfw3.lib")
 
 using namespace glm;
+
+Camera camera;
+
+float lastFrame = 0.0f;
+float deltaTime = 0.0f;
 
 void FrameBufferSizeCallback(GLFWwindow* window, int w, int h)
 {
@@ -21,11 +30,66 @@ void InputProcess(GLFWwindow* window)
 	{
 		glfwSetWindowShouldClose(window, GLFW_TRUE);
 	}
+
+	deltaTime = glfwGetTime() - lastFrame;
+	lastFrame += deltaTime;
+
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	{
+		camera.ProcessKeyboard(LEFT, deltaTime);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	{
+		camera.ProcessKeyboard(RIGHT, deltaTime);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	{
+		camera.ProcessKeyboard(FORWARD, deltaTime);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	{
+		camera.ProcessKeyboard(BACKWARD, deltaTime);
+	}
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) 
+{
+	camera.ProcessMouseScroll(yoffset);
 }
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
-	// pass
+	static float lastX, lastY;
+	static float yaw = 0.0f, pitch = 0.0f;
+
+	static bool firstMouse = true;
+	if (firstMouse) {  //设置初始位置，防止突然跳到某个方向上
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = lastX - xpos;   //别忘了，在窗口中，左边的坐标小于右边的坐标，而我们需要一个正的角度
+	float yoffset = lastY - ypos;   //同样，在窗口中，下面的坐标大于上面的坐标，而我们往上抬头的时候需要一个正的角度
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.5f;  //旋转精度
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	if (pitch > 89.0f)  //往上看不能超过90度
+		pitch = 89.0f;
+	if (pitch < -89.0f)  //往下看也不能超过90度
+		pitch = -89.0f;
+
+	camera.ProcessMouseMovement(xoffset, yoffset, pitch);
 }
 
 constexpr unsigned SCR_WIDTH = 1280;
@@ -49,6 +113,10 @@ int main(int argc, char* argv[])
 	}
 
 	glfwMakeContextCurrent(window); // 将窗口的上下文环境设置为主线程的上下文环境。
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
 
 	// 初始化glad
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -135,15 +203,20 @@ int main(int argc, char* argv[])
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	mat4 view = lookAt(vec3(0, 0, 5), vec3(0, 0, -1), vec3(0, 1, 0));
-
 	mat4 lightCubeModel;
 	lightCubeModel = translate(lightCubeModel, vec3(0, 0, 0));
+	lightCubeModel = glm::rotate(lightCubeModel, glm::radians(45.0f), glm::vec3(1.0f, 1.0f, 0.0f));
 
 	mat4 lampCubeModel;
 	lampCubeModel = translate(lampCubeModel, vec3(1.2f, 1.0f, 2.0f));
+	lampCubeModel = glm::scale(lampCubeModel, glm::vec3(0.2f));
 
-	mat4 projection = perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+	
+
+	glEnable(GL_DEPTH_TEST);
+
+	deltaTime = glfwGetTime();
+	lastFrame = deltaTime;
 
 	// 渲染循环
 	while (!glfwWindowShouldClose(window))
@@ -154,9 +227,26 @@ int main(int argc, char* argv[])
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		
-		
+		mat4 view = camera.GetViewMatrix();
+		mat4 projection = perspective(camera.Zoom, (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
+		lightShader.Use();
+		glBindVertexArray(VAO);
+		lightShader.SetVec3("objectColor", 0.4, 0.1, 0.1);
+		lightShader.SetVec3("lightColor", 1.0f, 1.0f, 1.0f);
+		lightShader.SetMat4("model", glm::value_ptr(lightCubeModel));
+		lightShader.SetMat4("view", glm::value_ptr(view));
+		lightShader.SetMat4("projection", glm::value_ptr(projection));
+
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		lampShader.Use();
+		glBindVertexArray(VAO);
+		lampShader.SetMat4("model", glm::value_ptr(lampCubeModel));
+		lampShader.SetMat4("view", glm::value_ptr(view));
+		lampShader.SetMat4("projection", glm::value_ptr(projection));
+
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents(); // 轮询事件
